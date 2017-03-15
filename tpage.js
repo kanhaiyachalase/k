e8e7842f881f0db871e0000ebb55a9c9ec535011
kushmisra <kushmisra2609@gmail.com>
2017-03-16 05:14:08 +0530

    better

--- tpage.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("tpage").style.height=screen.height*(1.3) + "px";
-
-document.getElementById("skilltitle").style.paddingTop=screen.height*(0.8)*(0.1)+"px";
-document.getElementById("skilltitle").style.paddingLeft=screen.width*(0.49)*(0.1)+"px";
-document.getElementById("skilltitle").style.paddingRight=screen.width*(0.49)*(0.1)+"px";
-
-document.getElementById("skill1").style.height=screen.height*(0.6)*(0.1) + "px";
