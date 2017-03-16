e9b41f6ae3a88078d11e12746d9dfe0c76fb94c4
kushmisra <kushmisra2609@gmail.com>
2017-03-16 07:15:50 +0530

    bbr

--- spage.js
+++ spage.js
@@ -17,6 +17,9 @@ ctx.lineWidth=2;
 ctx.strokeStyle="rgba(255,255,255,0.7)";
 ctx.arc(150, 75, 50, -Math.PI/2, Math.PI/2);
 ctx.stroke();
+ctx.strokeStyle = 'white';
+ctx.font = '18px Arial';
+ctx.strokeText('50%', 130, 80);
 
 
 var ctx = sc.getContext("2d");
@@ -25,6 +28,10 @@ ctx.lineWidth=2;
 ctx.strokeStyle="rgba(255,255,255,0.7)";
 ctx.arc(150, 75, 50, 0, 2*Math.PI);
 ctx.stroke();
+ctx.strokeStyle = 'white';
+ctx.font = '18px Arial';
+ctx.strokeText('100%', 130, 80);
+
 
 
 var ccd = document.getElementById("ccd");
